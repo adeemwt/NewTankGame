8901af14e423bb85ea8a0465981fc8efd796f765
adeem <admwt55@gmail.co>
2016-09-24 11:22:43 +0300

    small changes

--- app/src/main/java/com/example/adeem/tankgame/dbInitialConnect.java
+++ app/src/main/java/com/example/adeem/tankgame/dbInitialConnect.java
@@ -5,7 +5,7 @@ import com.firebase.client.Firebase;
 /**
  * Created by adeem on 16/09/2016.
  */
-public class crowdWeather extends android.app.Application{
+public class dbInitialConnect extends android.app.Application{
     @Override
     public void onCreate() {
         super.onCreate();
