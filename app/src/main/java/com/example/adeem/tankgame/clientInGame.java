fe443576c7f202b4fa1827dc158d844d14556cd9
ITSIK <itsik1231995@gmail.com>
2016-11-06 12:14:09 +0200

    mm

--- app/src/main/java/com/example/adeem/tankgame/clientInGame.java
+++ app/src/main/java/com/example/adeem/tankgame/clientInGame.java
@@ -603,7 +603,7 @@ public class clientInGame extends AppCompatActivity implements View.OnClickListe
                             }
 
                             contex.test.setText(contex.test.getText()+"\nmoved : " + msg.get(0).getPosition().x + " , " + msg.get(0).getPosition().y);
-
+                            msg=null;
                             //contex.settext_( "\nmoved : " + myenemy.get(1).getX() + " , " + myenemy.get(1).getY());//try it now . if we get s
                         }
                     });
